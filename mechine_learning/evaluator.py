11f5eea5afddbb70fc3f346b215a83dc1fdea99c
151250176 <151250176@smail.nju.edu.cn>
2017-08-03 16:59:00 +0800

    add machine_learning part including evaluator and logistic regression

---
+++ mechine_learning/evaluator.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+# predict_method: what you get in learning
+# data: the data to predict
+# target: the expectation value
+# beta: use to caculate F1
+#       beta > 1 means recall is more important
+#       beta < 1 means precision is more important
+def classification_evaluator(predict_method, data, target, beta=1):
+    # parameter check
+    if len(data) != len(target):
+        raise ValueError(
+            "data length isn't equal to target length: data len:" + str(len(data)) + " target len:" + str(len(target)))
+    # main part
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    total_num = len(data)
+
+    for i in range(total_num):
+        data_temp = data[i].reshape(1, -1)
+        if target[i] == 1:
+            tp += int((predict_method(data_temp) == target[i]))
+            fn += int((predict_method(data_temp) != target[i]))
+        else:
+            tn += int((predict_method(data_temp) == target[i]))
+            fp += int((predict_method(data_temp) != target[i]))
+
+    precision = tp / (tp + fp)  # 查准率
+    recall = tp / (tp + fn)  # 查全率
+    error_rate = (fp + fn) / total_num  # 错误率
+    F1 = (1 + beta ** 2) * precision * recall / (beta ** 2 * precision + recall)
+    TPR = tp / (tp + fn)  # true positive rate
+    FPR = fp / (tn + fp)  # false positive rate
+
+    return [error_rate, precision, recall, F1, TPR, FPR]
+
+
+# predict_method: what you get in learning
+# data: the data to predict
+# target: the expectation value
+def regression_evaluator(predict_method, data, target):
+    # parameter check
+    if len(data) != len(target):
+        raise ValueError(
+            "data length isn't equal to target length: data len:" + str(len(data)) + " target len:" + str(len(target)))
+    # main part
+    total_num = len(data)
+    error = []
+    for i in range(total_num):
+        data_temp = data[i].reshape(1, -1)
+        error.append(predict_method(data_temp) - target[i])
+
+    return [np.mean(error), np.std(error), total_num]
---
+++ mechine_learning/logic_regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+
+from mechine_learning.evaluator import classification_evaluator
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+s1 = np.arange(25, 50)
+s2 = np.arange(75, 100)
+X = X[y != 2]
+X1 = np.append(X[25:50], X[75:100], axis=0)
+y = y[y != 2]
+y1 = np.append(y.take(s1), y.take(s2))
+
+clf = LogisticRegression(C=1, penalty='l1', tol=1e-4)
+clf.fit(X1, y1)
+print(np.append(X[0:25], X[50:75], axis=0))
+print(classification_evaluator(clf.predict, np.append(X[0:25], X[50:75], axis=0), np.append(y[0:25], y[50:75], axis=0)))
